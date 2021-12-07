6ae9f0755d73ecfe409c1f41705807725af3b9ba
luncliff <luncliff@gmail.com>
2021-12-08 03:37:18 +0900

    update conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,4 @@
-#
 # https://docs.conan.io/en/latest/creating_packages/getting_started.html
-# https://spdx.org/licenses/
-#
 from conans import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake
 from conan.tools.layout import cmake_layout
@@ -9,15 +6,15 @@ from conan.tools.layout import cmake_layout
 
 class SsfConan(ConanFile):
     name = "ssf"
-    version = "1.0.2"
+    version = "1.0.3"
     description = "Simple Socket Functions"
     url = "https://github.com/luncliff/ssf"
-    license = "CC-BY-4.0"
+    license = "CC-BY-4.0"  # https://spdx.org/licenses/
 
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
 
-    generators = ["cmake_find_package", "cmake_paths"]
+    generators = "cmake_find_package"
     # requires = [
     #     "catch2/2.13.0"
     # ]
@@ -29,25 +26,36 @@ class SsfConan(ConanFile):
         "src/socket.hpp", "src/socket.cpp", "src/socket_test.cpp",
     ]
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            self.options.shared = True
-
+    # https://docs.conan.io/en/latest/developing_packages/package_layout.html
     def layout(self):
         cmake_layout(self)
 
+    # https://docs.conan.io/en/latest/reference/conanfile/tools/cmake/cmaketoolchain.html
     def generate(self):
-        toolchain = CMakeToolchain(self)
-        toolchain.generate()
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTING"] = "ON"
+        tc.generate()
 
+    # https://docs.conan.io/en/latest/reference/build_helpers/cmake.html
     def build(self):
         cmake = CMake(self)
+        if self.settings.os == "Windows":
+            cmake.append_vcvars = True
         cmake.configure()
         cmake.build()
 
+    def test(self):
+        cmake = CMake(self)
+        if self.settings.os == "Windows":
+            cmake.append_vcvars = True
+        cmake.test()
+
     def package(self):
         cmake = CMake(self)
+        if self.settings.os == "Windows":
+            cmake.append_vcvars = True
         cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["ssf"]
+        self.cpp_info
